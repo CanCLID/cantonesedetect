e1310ba85137eeb47c2a1f6da3ab99528e071b4e
Chaak Ming Lau <chaakming@gmail.com>
2024-04-06 21:40:57 +0800

    Implement Quotes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cantonesedetect',
+    version='0.1',
+    description='A minimal package that detect Cantonese sentences in Traditional Chinese text.',
+    author='Chaak Ming Lau, Mingfei Lau and Ann Wai Huen To',
+    packages=find_packages(),
+)
