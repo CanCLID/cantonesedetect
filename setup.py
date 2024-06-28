1d370a9b9059c6d2697e21aec4292cf46b71b390
Chaak Ming Lau <chaakming@gmail.com>
2024-06-28 18:55:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cantonesedetect',
-    version='0.1',
+    version='1.0',
     description='A minimal package that detect Cantonese sentences in Traditional Chinese text.',
     author='Chaak Ming Lau, Mingfei Lau and Ann Wai Huen To',
     packages=find_packages(),
