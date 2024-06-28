35a9bc581b88064f9d415a2caedc678df7b487f6
Chaak Ming Lau <chaakming@gmail.com>
2024-06-28 21:42:34 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='cantonesedetect',
     version='1.0',
